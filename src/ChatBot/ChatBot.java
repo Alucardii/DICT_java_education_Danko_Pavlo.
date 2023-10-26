7b1f6e224e436059e7c8db7f9e8c6823353c39fc
Alucardii <p.y.danko@student.khai.edu>
2023-10-26 14:56:01 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
-        // Определіть ім'я бота і рік його створення
+
         String botName = "Botik";
         int birthYear = java.time.Year.now().getValue();
 
-        // Виведіть привітання
+
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, remind me your name.");
+        String yourName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + yourName + "!");
     }
 }
